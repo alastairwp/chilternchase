b94609496dc971f3b5ade958c546003f325facad
Alastair Williamson-Pound <alastair.williamson-pound@alastairs-mbp.local>
2023-02-22 20:45:07 +0000

    cancelled message

--- src/src/settings.py
+++ src/src/settings.py
@@ -28,13 +28,16 @@ SECRET_KEY = os.environ.get('CC_SECRET_KEY')
 
 # *******************************
 # Live settings
-DEBUG = False
-ALLOWED_HOSTS = os.environ.get('CC_DJANGO_ALLOWED_HOSTS').split(',')
+# DEBUG = False
+# ALLOWED_HOSTS = os.environ.get('CC_DJANGO_ALLOWED_HOSTS').split(',')
 # *******************************
+
+
+# *******************************
+# Dev settings
 DEBUG = True
-
-# ALLOWED_HOSTS = [os.environ.get('CC_DJANGO_ALLOWED_HOSTS').split(',')]
-
+ALLOWED_HOSTS = ['*']
+# *******************************
 
 # Application definition
 
@@ -96,7 +99,7 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'src.wsgi.application'
 
-"""
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
@@ -116,7 +119,7 @@ DATABASES = {
         'PORT': os.environ.get('CC_DB_PORT'),
     }
 }
-
+"""
 # import dj_database_url
 # DATABASES['default'] = dj_database_url.config()
 
