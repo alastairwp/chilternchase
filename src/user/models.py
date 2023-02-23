520d50115bf43317ddf265d72bcc042fd06c0c12
Alastair Williamson-Pound <alastair.williamson-pound@alastairs-mbp.fritz.box>
2023-02-23 12:59:42 +0000

    fix deployment

--- src/user/models.py
+++ src/user/models.py
@@ -6,7 +6,7 @@ from django.db import models
 from django.contrib.auth.models import BaseUserManager
 from django.contrib.auth.models import AbstractBaseUser
 from django.contrib.auth.models import PermissionsMixin
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 
 
 class MyUserManager(BaseUserManager):
